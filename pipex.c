fc23f1ea1532a9fd69c9f30f51d2483093c076f9
mpeterson114 <mlpeterson114@gmail.com>
2023-07-26 17:44:52 +0200

    start

---
+++ pipex.c
@@ -0,0 +1,30 @@
+#include "pipex.h"
+
+int main(int argc, char *argv[], char *envp[])
+{
+    t_pipex pipex;
+
+    if(argc != 5)
+    {
+        //error function & message
+    }
+    pipex.infile = open(argv[1], O_RDONLY);
+    if (pipex.infile < 0)
+    {
+        //error function & message, exit
+    }
+    pipex.outfile = open(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR, 0777);  //permissions? test & compare w/shell command outputs later   
+    if (pipex.outfile < 0)
+    {
+        //error function & message, exit
+    }
+    if (pipe(pipex.end) < 0)
+        //error fxn-message, exit
+    
+    
+
+
+
+
+
+}
---
+++ pipex.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpeterso <mpeterso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 17:13:24 by mpeterso          #+#    #+#             */
+/*   Updated: 2023/07/26 17:28:09 by mpeterso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <fcntl.h>
+# include <string.h>
+# include "./gnl/get_next_line.h"
+
+typedef struct s_pipex
+{
+    pid_t pid1;
+    pid_t pid2;
+    int end[2];
+    int infile;
+    int outfile;
+    char *path;
+    char **cmd_paths;
+    char **cmd_args;
+    char *cmd;
+} t_pipex;
+
+
+
+
+
+#endif
